6255dfec24fcfbf6ad11943f04d06279e8449ec2
songyiwei <349077029@qq.com>
2016-02-18 14:46:26 +0800

    更新修改数据库HOST的方法

--- lib/modifyDBurl.sh
+++ lib/modifyDBurl.sh
@@ -2,10 +2,11 @@ function modifyDBurl {
 if [[ $DBUrl != "" ]]; then
     echo ""
     echo "Modify database URL ..."
+    DBNum=$(sed -n "/'default' => array/=" $ProjConfPath/database.php)
+    DBNum=$((${DBNum} + 1))
+    sed -i "${DBNum},/)/s/'host.*/'host' => '$DBUrl',/" $ProjConfPath/database.php
     cd $ProjPath
-    sed -i "s/'host'.*/'host' => '$DBUrl',/" $ProjConfPath/database.php
-    cd $ProjPath
-    ./script/phpmig status 1>/dev/null 2>&1
+    ENV=$ProjType ./script/phpmig status 1>/dev/null 2>&1
     if [[ $? == 0 ]]; then
         echo ""
         echo "Modify database URL is OK !"
@@ -14,6 +15,7 @@ if [[ $DBUrl != "" ]]; then
         echo "Modify database URL is Fail !"
         exit 1
     fi
+    unset DBNum
     cd - 1>/dev/null 2>&1
 fi
 }
