adf4849cd7397256e8fb2a12e944f302dbd80001
songyiwei <349077029@qq.com>
2016-02-18 14:54:17 +0800

    修改数据库HOST的格式

--- lib/modifyDBurl.sh
+++ lib/modifyDBurl.sh
@@ -4,7 +4,7 @@ if [[ $DBUrl != "" ]]; then
     echo "Modify database URL ..."
     DBNum=$(sed -n "/'default' => array/=" $ProjConfPath/database.php)
     DBNum=$((${DBNum} + 1))
-    sed -i "${DBNum},/)/s/'host.*/'host' => '$DBUrl',/" $ProjConfPath/database.php
+    sed -i "${DBNum},/)/s/'host.*/'host'     => '$DBUrl',/" $ProjConfPath/database.php
     cd $ProjPath
     ENV=$ProjType ./script/phpmig status 1>/dev/null 2>&1
     if [[ $? == 0 ]]; then
