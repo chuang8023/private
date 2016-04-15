e3df00bba3d9d33dccec61c7bb73e4e11f041f5a
songyiwei <349077029@qq.com>
2016-04-15 16:18:59 +0800

    将管理临时实例php脚本扩充为接受参数

--- lib/manageTempDB.php
+++ lib/manageTempDB.php
@@ -9,6 +9,7 @@ require __DIR__ . '/aliyunAPI.php';
  $secret = 'o8CLhEK6k3iyrjYPmL3TlTBBtK3I9y';
  $dbId = 'rds26h77108r3o8c4jwj';
  $host = 'https://rds.aliyuncs.com/';
+ $format = 'JSON';          //请勿随意修改
  
  /*
   * RDS API文档地址 https://help.aliyun.com/document_detail/rds/OpenAPI-manual/RDS-OpenAPI-Invoke/rquest-structure.html?spm=5176.product8314883_rds.6.213.0gvWN2
@@ -48,98 +49,160 @@ require __DIR__ . '/aliyunAPI.php';
   * BackupId    //备份ID
   */
  
-function DescribeDBInstanceAttribute ($key, $secret, $host, $dbId, $format='JSON') {
-    $getDBId = AliyunAPI(                                                    //调用AliyunAPI.php的AliyunAPI函数
-    $key,
-    $secret,
-    $host,
-    $format,
-    array(
-        'Action' => 'DescribeDBInstanceAttribute',
-        'DBInstanceId' => $dbId,
-    )
+function GetDBInfo($dbId) {
+    $dbInfo = AliyunAPI(                                                    //调用AliyunAPI.php的AliyunAPI函数
+    $GLOBALS['key'],
+    $GLOBALS['secret'],
+    $GLOBALS['host'],
+    $GLOBALS['format'],
+        array(
+            'Action' => 'DescribeDBInstanceAttribute',
+            'DBInstanceId' => $dbId,
+        )
     );
-    return $getDBId;
+    return $dbInfo;
 }
 
-function DeleteDBInstance ($key, $secret, $host, $dbId, $format='JSON') {
+function GetTempDBId($dbInfo) {
+    if (isset($dbInfo['Items']['DBInstanceAttribute']['0']['TempDBInstanceId']) && !empty($dbInfo['Items']['DBInstanceAttribute']['0']['TempDBInstanceId'])) {
+    	$tempDBId = $dbInfo['Items']['DBInstanceAttribute']['0']['TempDBInstanceId'];
+    } else {
+        $tempDBId = '';
+    }
+    return $tempDBId;
+}
+
+function GetDBExpireTime($dbInfo) {
+    if (isset($dbInfo['Items']['DBInstanceAttribute']['0']['ExpireTime']) && !empty($dbInfo['Items']['DBInstanceAttribute']['0']['ExpireTime'])) {
+        $dbExpireTime = $dbInfo['Items']['DBInstanceAttribute']['0']['ExpireTime'];
+    } else {
+        $dbExpireTime = '';
+    }
+    return $dbExpireTime;
+}
+
+function DeleteDB($dbId) {
     if (!preg_match("/^sub/i", $dbId)) {                                    //检测数据库ID是否以sub开头，如果不是拒绝执行删除操作并退出
         echo "要求删除一个非临时实例,默认拒绝!";
         exit(1);
     }
     $deleteResult = AliyunAPI(                                                //调用AliyunAPI.php的AliyunAPI函数
-    $key,
-    $secret,
-    $host,
-    $format,
-    array(
-        'Action' => 'DeleteDBInstance',
-        'DBInstanceId' => $dbId,
-    )
+    $GLOBALS['key'],
+    $GLOBALS['secret'],
+    $GLOBALS['host'],
+    $GLOBALS['format'],
+        array(
+            'Action' => 'DeleteDBInstance',
+            'DBInstanceId' => $dbId,
+        )
     );
     return $deleteResult;
 }
 
-function DescribeBackups ($key, $secret, $host, $dbId, $format='JSON') {
+function GetBackupId($dbId) {
     $startTime = gmdate('Y-m-d\TH:i\Z',time() - 7 * 24 * 60 * 60);         //7天内
     $endTime = gmdate('Y-m-d\TH:i\Z');
-    $getBackupId = AliyunAPI(                                                //调用AliyunAPI.php的AliyunAPI函数
-    $key,
-    $secret,
-    $host,
-    $format,
-    array(
-        'Action' => 'DescribeBackups',
-        'DBInstanceId' => $dbId,
-        'StartTime' => $startTime,
-        'EndTime' => $endTime
-    )
+    $backupResult = AliyunAPI(                                                //调用AliyunAPI.php的AliyunAPI函数
+    $GLOBALS['key'],
+    $GLOBALS['secret'],
+    $GLOBALS['host'],
+    $GLOBALS['format'],
+        array(
+            'Action' => 'DescribeBackups',
+            'DBInstanceId' => $dbId,
+            'StartTime' => $startTime,
+            'EndTime' => $endTime
+        )
     );
-    return $getBackupId;
+    $backupStatus = $backupResult['Items']['Backup']['0']['BackupStatus'];
+    if ($backupStatus == 'Success') {                                         //如果第一个备份状态不成功，则使用第二个备份
+        $backupId = $backupResult['Items']['Backup']['0']['BackupId'];
+    } else {
+        $backupId = $backupResult['Items']['Backup']['1']['BackupId']; 
+    }
+    return $backupId;
 }
 
-function CreateTempDBInstance ($key, $secret, $host, $dbId, $format='JSON') {
-    $getBackupId = DescribeBackups($key, $secret, $host, $dbId);
-    $backupStatus = $getBackupId['Items']['Backup']['0']['BackupStatus'];
-    if ($backupStatus == 'Success') {                                         //如果第一个备份状态不成功，则使用第二个备份
-        $backupId = $getBackupId['Items']['Backup']['0']['BackupId'];
-    } else {
-        $backupId = $getBackupId['Items']['Backup']['1']['BackupId']; 
-    }
-    $getTempDBId = AliyunAPI(                                               //调用AliyunAPI.php的AliyunAPI函数
-    $key,
-    $secret,
-    $host,
-    $format,
-    array(
-        'Action' => 'CreateTempDBInstance',
-        'DBInstanceId' => $dbId,
-        'BackupId' => $backupId
-    )
+function CreateTempDB($dbId) {
+    $backupId = GetBackupId($dbId);
+    $createResult = AliyunAPI(                                               //调用AliyunAPI.php的AliyunAPI函数
+    $GLOBALS['key'],
+    $GLOBALS['secret'],
+    $GLOBALS['host'],
+    $GLOBALS['format'],
+        array(
+            'Action' => 'CreateTempDBInstance',
+            'DBInstanceId' => $dbId,
+            'BackupId' => $backupId
+        )
     );
-    return $getTempDBId;
+    if (isset($createResult['TempDBInstanceId']) && !empty($createResult['TempDBInstanceId'])) {
+        $tempDBId = $createResult['TempDBInstanceId'];
+    } else {
+        echo $createResult['Message'] . "\n";
+        exit(1);
+    }
+    return $tempDBId;
 }
 
 //------------------------------------------------------------------------------------------------------------------------
- 
-$getTempDBId = DescribeDBInstanceAttribute($key, $secret, $host, $dbId);
-if (isset($getTempDBId['Items']['DBInstanceAttribute']['0']['TempDBInstanceId']) && !empty($getTempDBId['Items']['DBInstanceAttribute']['0']['TempDBInstanceId'])) {
-    $tempDBId = $getTempDBId['Items']['DBInstanceAttribute']['0']['TempDBInstanceId'];
-    $getTempDBExpireTime = DescribeDBInstanceAttribute($key, $secret, $host, $tempDBId);
-    $tempDBExpireTime = $getTempDBExpireTime['Items']['DBInstanceAttribute']['0']['ExpireTime'];
 
-    $surplus = strtotime($tempDBExpireTime) - strtotime(gmdate('Y-m-d\TH:i:s\Z'));
-
-    if (($surplus < 12 * 60 * 60) && (gmdate('H') > 12 && gmdate('H') < 22)) {    //如果离过期时间不足12个小时，且在北京时间20点到第二天凌晨6点之间
-        DeleteDBInstance($key, $secret, $host, $tempDBId);
-        $getTempDBId = CreateTempDBInstance ($key, $secret, $host,$dbId);
-        $tempDBId = $getTempDBId['TempDBInstanceId'];
-        echo $tempDBId;
+function AutoManage($dbId) {
+    $dbInfo = GetDBInfo($dbId);
+    $tempDBId = GetTempDBId($dbInfo);
+    if (!empty($tempDBId)) {
+        $tempDBInfo = GetDBInfo($tempDBId);
+        $tempDBExpireTime = GetDBExpireTime($tempDBInfo);
+        if (!empty($tempDBExpireTime)) {
+            $surplus = strtotime($tempDBExpireTime) - strtotime(gmdate('Y-m-d\TH:i:s\Z'));
+        } else {
+            echo "Cannot get temp database expire time !\n";
+            exit(1);
+        }
+        if (($surplus < 12 * 60 * 60) && (gmdate('H') > 12 && gmdate('H') < 22)) {    //如果离过期时间不足12个小时，且在北京时间20点到第二天凌晨6点之间
+            DeleteDB($tempDBId);
+            $tempDBId = CreateTempDB($dbId);
+            echo $tempDBId . "\n";
+        }
+    } else {
+        $tempDBId = CreateTempDB($dbId);
+        echo $tempDBId . "\n";
     }
-} else {
-    $getTempDBId = CreateTempDBInstance ($key, $secret, $host, $dbId);
-    $tempDBId = $getTempDBId['TempDBInstanceId'];
-    echo $tempDBId;
 }
 
-
+if (isset($argv['1']) && !empty($argv['1'])) {
+    switch ($argv['1']) {
+        case 'expireTime':
+            $dbInfo = GetDBInfo($dbId);
+            $tempDBId = GetTempDBId($dbInfo);
+            if (!empty($tempDBId)) {
+                $tempDBInfo = GetDBInfo($tempDBId);
+                $tempDBExpireTime = GetDBExpireTime($tempDBInfo);
+                echo gmdate('Y-m-d H:i:s', strtotime($tempDBExpireTime) + 8 * 60 * 60) ."\n";
+            } else {
+                echo "Cannot get temp database instance ID !\n";
+                exit(1);
+            }
+            break;
+        case 'deleteTempDB':
+            $dbInfo = GetDBInfo($dbId);
+            $tempDBId = GetTempDBId($dbInfo);
+            if (!empty($tempDBId)) {
+                DeleteDB($tempDBId);
+                echo "Delete temp database is OK !\n";
+            } else {
+                echo "Cannot get temp database ID !\n";
+                exit(1);
+            }
+            break;
+        case 'createTempDB':
+            $tempDBId = CreateTempDB($dbId);
+            echo $tempDBId . "\n";
+            break;
+        default :
+            echo "Error argvs !\n";
+            exit(1);
+    }
+} else {
+    AutoManage($dbId);
+}
