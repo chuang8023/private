68698fb15b46dc06cad3123090668c574652696b
wkjun <wkjun@foxmail.com>
2016-04-27 21:36:08 +0800

    Demo部署中文提示修改为英文

--- ext/modify_demo.sh
+++ ext/modify_demo.sh
@@ -3,7 +3,7 @@ DemoName=$1
 Operation=$2
 TittleName=$3
 
-  [[ $DemoName = '' || $Operation = '' ]] && echo "输入参数错误" && exit 1
+  [[ $DemoName = '' || $Operation = '' ]] && echo "Error Parameter !" && exit 1
 
      case $DemoName in 
 	yhpc)
@@ -15,18 +15,18 @@ TittleName=$3
 
 		  sed -i "s/项目名/$TittleName/" /home/anyuan/DemoData/$DemoDir/config/development/app.php
 		  sed -i "s/项目名/$TittleName/" /home/anyuan/DemoData/$DemoDir/public/layout/layout.js
-		  echo "项目名更新成功！"
+		  echo "Project's name update successfully！"
 		  ;;
 		  rebjpic)
                  scp anyuan@192.168.0.215:/home/anyuan/sa_dir/demodata/$DemoDir/bj.png  /home/anyuan/DemoData/$DemoDir/public/home/themes/nmgyh/images/bj3.png
-                  echo "背景图片更新成功！"
+                  echo "Project's background picture update successfully！"
 		  ;;
 		  redzpic)
                  scp anyuan@192.168.0.215:/home/anyuan/sa_dir/demodata/$DemoDir/dz.png  /home/anyuan/DemoData/$DemoDir/public/home/themes/nmgyh/images/dizi.png
-                  echo "底部技术支持图片更新成功！"
+                  echo "Project's bottom picture update successfully！"
 		  ;;
 		   *)
-		  echo "参数输入错误";exit 1
+		  echo "Error Parameter !";exit 1
 	     esac
 		 ;;
 		 ajap)
@@ -35,10 +35,10 @@ TittleName=$3
 		       rename)
                              cp /home/anyuan/template/$DemoDir/app.php /home/anyuan/DemoData/$DemoDir/config/development/app.php
                              sed -i "s/项目名/$TittleName/" /home/anyuan/DemoData/$DemoDir/config/development/app.php
-		             echo "项目名更新成功！"
+		             echo "Project's name update successfully！"
                              ;;
 			    *)
-			     echo "参数输入错误";exit 1
+			     echo "Error Parameter !";exit 1
 		  esac
 		   ;;
 		 zyrj)
@@ -47,12 +47,12 @@ TittleName=$3
                        rename)
                              cp /home/anyuan/template/$DemoDir/app.php /home/anyuan/DemoData/$DemoDir/config/development/app.php
                              sed -i "s/项目名/$TittleName/" /home/anyuan/DemoData/$DemoDir/config/development/app.php
-		             echo "项目名更新成功！"
+		             echo "Project's name update successfully！"
                              ;;
                             *)
-                             echo "参数输入错误";exit 1
+                             echo "Error Parameter !";exit 1
                   esac
                    ;;
 		  *)
-		echo "参数输入错误";exit 1
+		echo "Error Parameter !";exit 1
      esac
