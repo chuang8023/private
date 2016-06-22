babc020a015f5fc69de2c3373b6f9327101b8e42
Jack Jun <>
2016-06-22 11:21:04 +0800

    修复参数错误

--- lib/redis.sh
+++ lib/redis.sh
@@ -1,8 +1,8 @@
 function CleanRedis () {
 echo "clean the project redis cache now"
-AppNamePath="$ProPath/config/$ProjType/app.php"
+AppNamePath="$ProjPath/config/$ProjType/app.php"
 AppName=`cat $AppNamePath|grep application_name|awk -F "=>" '{print $2}'|sed 's/,//'`
-RedisPassPath="$ProPath/config/$ProjType/redis.php"
+RedisPassPath="$ProjPath/config/$ProjType/redis.php"
 RedisPass=`cat $RedisPassPath|grep auth|awk -F "," '{print $1}'|awk -F "=>" '{print $2}'`
 redis-cli -p 6379 -a $RedisPass keys "$AppName*" | xargs redis-cli -p 6379 -a  $RedisPass del >> /dev/null
  if [[ $? == 0 ]]; then
