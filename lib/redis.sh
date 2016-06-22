fc02380190161ef71b7e81c986ed4ba11ff0d96c
Jack Jun <>
2016-06-22 11:34:07 +0800

    redis服务器地址

--- lib/redis.sh
+++ lib/redis.sh
@@ -4,7 +4,8 @@ AppNamePath="$ProjPath/config/$ProjType/app.php"
 AppName=`cat $AppNamePath|grep application_name|awk -F "=>" '{print $2}'|sed 's/,//'`
 RedisPassPath="$ProjPath/config/$ProjType/redis.php"
 RedisPass=`cat $RedisPassPath|grep auth|awk -F "," '{print $1}'|awk -F "=>" '{print $2}'`
-redis-cli -p 6379 -a $RedisPass keys "$AppName*" | xargs redis-cli -p 6379 -a  $RedisPass del >> /dev/null
+RedisHost=`cat $RedisPassPath|grep default|awk -F "=>" '{print $2}'|sed 's/,//'|sed 's/:6379//'`
+redis-cli -p 6379 -h $RedisHost -a $RedisPass keys "$AppName*" | xargs redis-cli -p 6379 -h $RedisHost -a  $RedisPass del >> /dev/null
  if [[ $? == 0 ]]; then
         echo ""
         echo "clean the project redis cache is ok !"
