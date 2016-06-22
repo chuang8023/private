bfa0b1067395b40804c02f8154b236d7d132518b
Jack Jun <>
2016-06-22 14:48:15 +0800

    修复redis 符号错误

--- lib/redis.sh
+++ lib/redis.sh
@@ -1,10 +1,10 @@
 function CleanRedis () {
-echo "clean the project redis cache now"
+echo "clean the project redis cache now ..."
 AppNamePath="$ProjPath/config/$ProjType/app.php"
 AppName=`cat $AppNamePath|grep application_name|awk -F "=>" '{print $2}'|sed 's/,//'`
 RedisPassPath="$ProjPath/config/$ProjType/redis.php"
 RedisPass=`cat $RedisPassPath|grep auth|awk -F "," '{print $1}'|awk -F "=>" '{print $2}'`
-RedisHost=`cat $RedisPassPath|grep default|awk -F "=>" '{print $2}'|sed 's/,//'|sed 's/:6379//'`
+RedisHost=`cat $RedisPassPath|grep default|awk -F "=>" '{print $2}'|sed 's/,//'|sed 's/:6379//'|sed "s/'//g"`
 redis-cli -p 6379 -h $RedisHost -a $RedisPass keys "$AppName*" | xargs redis-cli -p 6379 -h $RedisHost -a  $RedisPass del >> /dev/null
  if [[ $? == 0 ]]; then
         echo ""
