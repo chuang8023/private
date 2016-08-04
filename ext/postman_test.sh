da291d47101c91dcc5dac84f43456a909b17dba6
Jack Jun <wkjun@foxmail.com>
2016-08-04 19:49:21 +0800

    调整postman逻辑

--- ext/postman_test.sh
+++ ext/postman_test.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+#0. 更新代码
+codedir=/var/www/www.ceshi.integration.aysaas.com
+branchname=integration
+cd $codedir
+git checkout .
+git pull --rebase origin $branchname 
+
 #1.数据库信息定义
+
 mysql_host="127.0.0.1"
-mysql_user="root"
-mysql_passwd="910629"
-mysql_dbname="db_apitestbase"
+mysql_user="saas_kawawa"
+mysql_passwd="hVxsR7VFYRKZsbNJ"
+mysql_dbname="ceshiintegration"
 
 mongo_host="127.0.0.1"
 mongo_user="root"
-mongo_passwd="910629"
-mongo_dbname="db_apitestbase"
+mongo_passwd="root"
+mongo_dbname="ceshiintegration"
+
 
 #2.设定备份路径 并确保路径存在
 mysql_dir="$USER/backup/mysql/"
@@ -22,23 +31,14 @@ if [ ! -d $mongo_dir ]; then
     mkdir -p $mongo_dir
 fi
 
-#3.备份/恢复 数据
+#3.备份数据
     #导出mongo
     echo '开始备份mongo'
     mongodump -h$mongo_host -u$mongo_user -p$mongo_passwd -d$mongo_dbname -o $mongo_dir
     echo '开始备份mysql'
     mysqldump -h$mysql_host -u$mysql_user -p$mysql_passwd  $mysql_dbname > $mysql_dir${mysql_dbname}.sql
 
-
-    #导入mongo
-    echo '开始恢复mongo'
-    mongorestore -h$mongo_host -u$mongo_user -p$mongo_passwd -d$mongo_dbname --drop ${mongo_dir}${mongo_dbname}
-    #导入mysql
-    echo '开始恢复mysql'
-    mysql -h$mysql_host -u$mysql_user -p$mysql_passwd $mysql_dbname < $mysql_dir${mysql_dbname}.sql
-
-
-#1.PostMan信息定义
+#4.PostMan信息定义
 createtime=`date +%Y-%m-%d-%H-%M`
 currentmonth=`date +%Y-%m`
 postman_json=/var/www/ApiTest/Json/ApiTest.json
@@ -50,3 +50,12 @@ if [ ! -d /var/www/ApiTest/Report/$currentmonth ]; then
 fi
 
 newman -c $postman_json -e $postman_env -H $postman_report
+
+#5.恢复数据
+
+    #导入mongo
+    echo '开始恢复mongo'
+    mongorestore -h$mongo_host -u$mongo_user -p$mongo_passwd -d$mongo_dbname --drop ${mongo_dir}${mongo_dbname}
+    #导入mysql
+    echo '开始恢复mysql'
+    mysql -h$mysql_host -u$mysql_user -p$mysql_passwd $mysql_dbname < $mysql_dir${mysql_dbname}.sql
