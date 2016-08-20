1d8053e76f7c985118a8c81ef8396778390586c5
Jack Jun <wkjun@foxmail.com>
2016-08-20 10:16:23 +0800

    日志精确时间

--- ext/git_merge.sh
+++ ext/git_merge.sh
@@ -4,7 +4,7 @@ function GitMerge ()
   FromBranch=$1
   ToBranch=$2
   Repertory=$3
-  Date=`date +%Y-%m-%d`
+  Date=`date +%Y-%m-%d-%H:%M`
   echo "start to merge $FromBranch  to $ToBranch !"
   git checkout $FromBranch
   git pull --rebase origin $FromBranch
