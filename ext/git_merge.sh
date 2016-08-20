25a3ff4fd083e7f7a028e0c8c81a25e99726aae3
Jack Jun <wkjun@foxmail.com>
2016-08-20 10:11:28 +0800

    去除sleep选项

--- ext/git_merge.sh
+++ ext/git_merge.sh
@@ -6,7 +6,6 @@ function GitMerge ()
   Repertory=$3
   Date=`date +%Y-%m-%d`
   echo "start to merge $FromBranch  to $ToBranch !"
-  sleep 3 
   git checkout $FromBranch
   git pull --rebase origin $FromBranch
   git checkout $ToBranch
@@ -21,7 +20,6 @@ function GitMerge ()
             [ $HotfixRecord -eq 1 ] && echo "$Date : $HotFixAuthor : $HotFixBranch : $HotFixInfo" >> /$HOME/${Repertory}_master_merge_hotfix.log
          fi
      echo "start to push $ToBranch ...."
-     sleep 10
      git push origin $ToBranch:$ToBranch
     else
      echo ""
@@ -30,7 +28,6 @@ function GitMerge ()
      echo "请手动解决冲突后,再执行合并动作 !"
      echo ""
      echo "$Date : merge $FromBranch to $ToBranch is failed !" >> /$HOME/${Repertory}_merge_hotfix.log
-     sleep 10
      echo "$Date : merge $FromBranch to $ToBranch is failed ! please check hotfix merge locally !" | heirloom-mailx -s "hotfix auto merge results"  $EMail
      exit 1
  fi
