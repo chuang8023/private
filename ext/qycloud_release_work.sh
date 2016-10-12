c1d86f900480d7aa9eaa1cc4fbff19a910b9d288
Jack Jun <wkjun@foxmail.com>
2016-10-12 15:18:59 +0800

    发布日master打tag和切新master/hotfix函数分离

--- ext/qycloud_release_work.sh
+++ ext/qycloud_release_work.sh
@@ -1,14 +1,22 @@
 #/bin/bash
+ReleaseWorkFlow=$1
 SaaSRepertoryPath="$HOME/saas"
 cd $SaaSRepertoryPath
-TagName="master-v`date +%Y%m%d`"
-NewMasterHotfixName="hotfix/master-`date +%Y%m%d`"
-OldMasterHotfixName=`git branch|grep hotfix/master`
 #master打tag
+function MasterPickupTag () {
+TagName="master-v`date +%Y%m%d`"
 git checkout master
 git tag -a $TagName -m $TagName
 git push origin $TagName:$TagName
+}
 #新切推送master修复分支
+function CreateMasterHotfix () {
+NewMasterHotfixName="hotfix/master-`date +%Y%m%d`"
+OldMasterHotfixName=`git branch|grep hotfix/master`
 [ ! $OldMasterHotfixName == "" ] && git push origin :$OldMasterHotfixName
 git checkout -b $NewMasterHotfixName
 git push origin $NewMasterHotfixName:$NewMasterHotfixName
+}
+[ $ReleaseWorkFlow = "MasterPickupTag" ] && MasterPickupTag
+[ $ReleaseWorkFlow = "CreateMasterHotfix" ] && CreateMasterHotfix
+
