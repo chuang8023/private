b41330a8f5899b1f327b397b677ff9701d112aa2
Jack Jun <wkjun@foxmail.com>
2016-11-21 17:21:15 +0800

    “代码调试控制添加用户判断”

--- ext/CodeDebugControl.sh
+++ ext/CodeDebugControl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CodeMember=$1
 let "ExpiryDate=$2 * 60"
-
+cat /etc/vsftpd/ftpuser.txt|grep $CodeMember > /dev/null 2>&1
+[ ! $? -eq 0 ] && echo "研发账号不存在！" && exit 1
 echo $CodeMember >> /etc/vsftpd.user_allow
 sleep $ExpiryDate
 sed -i s/$CodeMember//g /etc/vsftpd.user_allow
