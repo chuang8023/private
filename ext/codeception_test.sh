3279d2286d00bc71ea04c2b1db7847147b3c56fb
端飞 <duanfeisj@sina.com>
2016-11-25 17:47:29 +0800

    new file codeception_test.sh

---
+++ ext/codeception_test.sh
@@ -0,0 +1,17 @@
+
+
+
+#拉取 tests  代码
+# ./script/tests unpackaging
+#
+
+#执行接口测试
+#log 地址 写入到 80端口可访问的地址，列表展现所有报表列表
+# 
+#  path = branchName_test_api_report.html
+#  env = intergration
+#  testPath = api
+#  group = step1
+#
+#./bin/codecept.phar run $testPath --env $env --html=$path
+
