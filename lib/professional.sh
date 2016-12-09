335cd8e309dc89db26f0df36190e54dadd2e1aef
Jack Jun <wkjun@foxmail.com>
2016-12-09 18:07:46 +0800

    移动professional 队列功能

--- lib/professional.sh
+++ lib/professional.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-. ../run.sh
-Param1=$1
-ProfessnalResque=$Param1
+function ProfessnalResque {
+ProfessnalResque=$$Param3
 Main
 cd $ProjPath
-case ProfessnalResque
+case $ProfessnalResque
  in 
   CatProfessnalResqueStatus) 
   php ./deploy/othersaas/getQueueStateProfessional.php
   ;;
   ResetProfessnalResque)
   php  ./deploy/othersaas/addQueueRedisCacheProfessional.php
-  ;;
- *)
 esac
+}
