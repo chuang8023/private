c6b9dd2a7b9d20e53d80e485d8e602b4640498a8
Jack Jun <wkjun@foxmail.com>
2017-01-11 21:39:06 +0800

    修复更新错误

--- ext/qycloud_release_work.sh
+++ ext/qycloud_release_work.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ReleaseWorkFlow=$1
 SaaSRepertoryPath="$HOME/saas"
-cd $SaaSRepertoryPath
 #master打tag
 function MasterPickupTag () {
+cd $SaaSRepertoryPath
 TagName="master-v`date +%y%m%d`"
 git checkout master
 git tag -a $TagName -m $TagName
@@ -12,6 +12,7 @@ exit 0
 }
 #新切推送master修复分支
 function CreateMasterHotfix () {
+cd $SaaSRepertoryPath
 NewMasterHotfixName="hotfix/master-`date +%y%m%d`"
 OldMasterHotfixName=`git branch|grep hotfix/master`
 OldMasterHotfixName=`echo $OldMasterHotfixName|sed 's/  //'|sed 's/* //'`
