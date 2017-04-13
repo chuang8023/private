2f13461eab9fea22f25fd957543e7a1bc2bf307c
Jack Jun <wkjun@foxmail.com>
2017-04-13 16:05:02 +0800

    新增打tag自动更新自动升级tag标记

--- ext/qycloud_release_work.sh
+++ ext/qycloud_release_work.sh
@@ -7,8 +7,18 @@ cd $SaaSRepertoryPath
 TagName="master-v`date +%y%m%d`"
 git checkout master
 git pull origin master
+git tag|grep $TagName
+[ $? -eq 0 ] && git push origin :$TagName && git tag -d $TagName
 git tag -a $TagName -m $TagName
 git push origin $TagName:$TagName
+CurrentMigrationTag=`ls -l db/migrations|awk '{print $9}'|awk -F '_' '{print $1}'|tail -1`
+LastMigrationTag=`cat deploy/othersaas/UpgradeData.php|grep master-v|awk -F '=>' '{print $1}'|sed "s/'//g"|tail -1|sed 's/ //g'`
+ if [ $CurrentMigrationTag != $LastMigrationTag ];then
+     sed -i  "/$LastMigrationTag/a\        '$CurrentMigrationTag' => '$TagName',"  deploy/othersaas/UpgradeData.php	
+     git add deploy/othersaas/UpgradeData.php
+     git commit -m "新增新$TagName标记"
+     git push origin master
+ fi
 exit 0
 }
 #新切推送master修复分支
