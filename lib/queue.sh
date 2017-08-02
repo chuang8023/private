da7169cc8d4405b49c3814104d545a2de627f1cc
huang <346619752@qq.com>
2017-08-02 15:30:17 +0800

    修改队列重启，先./deploy/queue重启，失败后重启supervisor守护进程

--- lib/queue.sh
+++ lib/queue.sh
@@ -28,15 +28,21 @@ local _Name=""
 echo "Restarting Queue ..."
 cd $ProjPath
     if [ $QueueName = "queue" ]; then
-	   rm -rf log/queue/*
-	   _Name=`cat config/$ProjType/app.php | grep "application_name" | awk '{print $3}' | sed "s/'//g" | sed "s/,//g"`
-	   rm -rf /etc/supervisor/conf.d/${_Name}_queue.conf
-	   ENV=$ProjType ./deploy/supervisor
-           ENV=$ProjType ./deploy/$QueueName stop 1>/dev/null << EOF  
+	  # rm -rf log/queue/*
+	  # _Name=`cat config/$ProjType/app.php | grep "application_name" | awk '{print $3}' | sed "s/'//g" | sed "s/,//g"`
+	  # rm -rf /etc/supervisor/conf.d/${_Name}_queue.conf
+	  # ENV=$ProjType ./deploy/supervisor
+           ENV=$ProjType ./deploy/$QueueName restart 1>/dev/null << EOF  
 Y
 EOF
-	   sudo -u $runuser supervisorctl stop all 1>/dev/null
-	   sudo -u $runuser supervisorctl restart all 1>/dev/null
+   	   if [ $? -eq 1 ];then
+	   	rm -rf log/queue/*
+	  	_Name=`cat config/$ProjType/app.php | grep "application_name" | awk '{print $3}' | sed "s/'//g" | sed "s/,//g"`
+	   	rm -rf /etc/supervisor/conf.d/${_Name}_queue.conf
+	   	ENV=$ProjType ./deploy/supervisor
+	   	sudo -u $runuser supervisorctl stop all 1>/dev/null
+	   	sudo -u $runuser service supervisor restart 1>/dev/null
+	   fi
       else
 	   sudo -u $runuser /usr/bin/env TERM=xterm ENV=$ProjType ./deploy/$QueueName stop 1>/dev/null
 	   sleep 2
