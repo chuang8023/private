f8cb036edcdfd4b19a7b3f4dc4630d1849fd7020
anyuan-sa <wkjun@foxmai'l.com>
2017-08-30 11:07:59 +0800

    修复pre自动合并机制的BUG

--- ext/preAutoMerge.sh
+++ ext/preAutoMerge.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if [[ $1 == "true" ]]; then
-    sed -i "s/^autoMergePre=.*/autoMergePre=true/" ../$(cd `dirname $0`;pwd)/hooks.sh
+    sed -i "s/^autoMergePre=.*/autoMergePre=true/" $(cd `dirname $0`;cd ..;pwd)/hooks.sh
+    if [[ $? == 0 ]]; then
+        echo ""
+        echo "已开启 - release合并到pre/qycloud的自动合并机制"
+    fi
 else
-    sed -i "s/^autoMergePre=.*/autoMergePre=false/" ../$(cd `dirname $0`;pwd)/hooks.sh
+    sed -i "s/^autoMergePre=.*/autoMergePre=false/" $(cd `dirname $0`;cd ..;pwd)/hooks.sh
+    if [[ $? == 0 ]]; then
+        echo "已关闭 - release合并到pre/qycloud的自动合并机制"
+    fi
 fi
